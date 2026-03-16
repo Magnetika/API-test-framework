a158b63c81bf16c487a3eefaa753e1fc3d78e2ee
Magnetika <agneslilla.szabo@gmail.com>
2026-03-16 13:12:45 +0100

    add jsonschema validation test

---
+++ tests/test_contract.py
@@ -0,0 +1,21 @@
+from api.client import APIClient
+from jsonschema import validate
+
+client = APIClient()
+
+user_schema = {
+    "type": "object",
+    "properties": {
+        "id": {"type": "integer"},
+        "email": {"type": "string"},
+        "first_name": {"type": "string"},
+        "last_name": {"type": "string"},
+        "avatar": {"type": "string"}
+    },
+    "required": ["id", "email", "first_name", "last_name", "avatar"]
+}
+
+def test_get_users_contract():
+    response = client.get_users()
+    data = response.json()["data"][0]
+    validate(instance=data, schema=user_schema)
